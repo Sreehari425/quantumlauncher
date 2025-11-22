ff47bdf3998f8658bae8ad859ead0aab66a07e8d
mrmayman <navneetkrishna22@gmail.com>
2025-11-22 14:11:51 +0530

    Dev: Update betterjsons list until 25w44a

--- tests/src/version.rs
+++ tests/src/version.rs
@@ -6,7 +6,7 @@ const fn ver(name: &'static str) -> Version {
     Version(name, &[])
 }
 
-const MODERN: [Loader; 2] = [Loader::Forge, Loader::Quilt];
+const FORGE_QUILT: [Loader; 2] = [Loader::Forge, Loader::Quilt];
 
 pub const VERSIONS_LWJGL2: &[Version] = &[
     // last version of classic, should represent most early versions
@@ -16,21 +16,23 @@ pub const VERSIONS_LWJGL2: &[Version] = &[
     // last based on old launcher system
     Version("1.5.2", &[Loader::Quilt]),
     // after migration to new launcher system
-    Version("1.7.10", &MODERN),
+    Version("1.7.10", &FORGE_QUILT),
     // one of the most popular release versions
-    Version("1.8.9", &MODERN),
+    Version("1.8.9", &FORGE_QUILT),
     // last version to use lwjgl2
-    Version("1.12.2", &MODERN),
+    Version("1.12.2", &FORGE_QUILT),
 ];
 
 pub const VERSIONS_LWJGL3: &[Version] = &[
-    ver("inf-20100415-lwjgl3"), // test of lwjgl3 backport
-    Version("1.14.4", &MODERN), // after migration to lwjgl3, engine rewrites
-    Version("1.16.5", &MODERN), // last version to use Java 8, OpenGL 2.x
+    ver("inf-20100415-lwjgl3"),      // test of lwjgl3 backport
+    Version("1.14.4", &FORGE_QUILT), // after migration to lwjgl3, engine rewrites
+    Version("1.16.5", &FORGE_QUILT), // last version to use Java 8, OpenGL 2.x
     // after migration to Java 17, OpenGL 3.x, engine rewrites
-    Version("1.18.2", &MODERN),
-    // last launchwrapper version
+    Version("1.18.2", &FORGE_QUILT),
+    // has some weird bugs
     Version("1.21.5", &[Loader::Forge, Loader::Quilt, Loader::Neoforge]),
-    // latest
+    // last launchwrapper version
     Version("1.21.10", &[Loader::Forge, Loader::Quilt, Loader::Neoforge]),
+    // latest
+    ver("1.21.11-pre2"),
 ];
