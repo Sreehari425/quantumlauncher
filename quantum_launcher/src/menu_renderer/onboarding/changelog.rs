d7ecbc570f76e9f628837500d5839e434fad48c4
mrmayman <navneetkrishna22@gmail.com>
2025-11-22 17:57:28 +0530

    Update changelog

--- quantum_launcher/src/menu_renderer/onboarding/changelog.rs
+++ quantum_launcher/src/menu_renderer/onboarding/changelog.rs
@@ -22,9 +22,10 @@ pub fn changelog<'a>() -> Element<'a> {
         widget::text("UX").size(32),
 
         widget::column![
+        "- The Create Instance screen now auto-fills the version and name by default.",
         "- Overhauled the mod list, now with ICONS, bulk-selection, and better aesthetics.",
-        "- Export mods as a text list for easy manual sharing, with optional links and instance details",
-        "- Tweaked and rearranged many menus/messages",
+        "- Export mods as a text list for easy manual sharing, with optional links and instance details.",
+        "- Tweaked and rearranged many menus/messages.",
         ].spacing(5),
 
         widget::image(IMG_MANAGE_MODS.clone()).height(400),
@@ -51,13 +52,13 @@ pub fn changelog<'a>() -> Element<'a> {
             widget::text("- Backspace no longer kills running instances without Ctrl.").size(14),
             widget::text("- Added warning if xrandr isn't installed").size(14),
             widget::Space::with_height(5),
-            widget::text("- Fixed the game log being a single-line mess ").size(14),
+            widget::text("- Fixed the game log being a single-line mess.").size(14),
             widget::text("- Fixed \"java binary not found\" macOS error.").size(14),
             widget::text("- Fixed crash with \"Better Discord Rich Presence\" mod.").size(14),
             widget::text("- Fixed launcher panic when launching the game.").size(14),
             widget::text("- Fixed NeoForge 1.21.1 and Forge 1.21.5 crash (reinstall loader to apply)").size(14),
             widget::text("- Fixed forge installer error: \"Processor failed, invalid outputs\"").size(14),
-            widget::text("- Fixed \"SSLHandshakeException\" crash on Windows").size(14),
+            widget::text("- Fixed \"SSLHandshakeException\" crash on Windows.").size(14),
         ].spacing(5) } else { widget::Column::new() },
 
         widget::Space::with_height(10),
