2b238882001f2abc1133aea2487c273679cae7cc
Void98 <shadow_pie99@proton.me>
2026-03-30 14:18:36 +0200

    Added changelog creator.

--- crates/ql_mod_manager/src/store/update.rs
+++ crates/ql_mod_manager/src/store/update.rs
@@ -1,9 +1,12 @@
 use std::sync::mpsc::Sender;
+use std::fs::create_dir_all;
+use std::fs::write;
 
 use chrono::DateTime;
+use chrono::Local;
 use ql_core::{GenericProgress, InstanceSelection, Loader, do_jobs, info, json::VersionDetails};
 
-use crate::store::{get_latest_version_date, toggle_mods};
+use crate::store::{get_info_bulk, get_latest_version_date, toggle_mods};
 
 use super::{ModError, ModId, ModIndex, delete_mods, download_mods_bulk};
 
@@ -24,12 +27,37 @@ pub async fn apply_updates(
 
     // It's as simple as that!
     delete_mods(updates.clone(), selected_instance.clone()).await?;
-    download_mods_bulk(updates, selected_instance.clone(), progress).await?;
+    download_mods_bulk(updates.clone(), selected_instance.clone(), progress).await?;
+
+    write_changelog(updates, selected_instance.clone()).await;
 
     // Ensure disabled mods stay disabled
     toggle_mods(disabled_mods, selected_instance).await?;
 
+
+
     Ok(())
+}
+
+
+async fn write_changelog(mod_id: Vec<ModId>, selected_instance: InstanceSelection) {
+
+    let titles = get_info_bulk(mod_id).await
+            .unwrap()
+            .into_iter()
+            .map(|n|n.title)
+            .collect::<Vec<_>>()
+            .join("\n");
+
+    let now = Local::now();
+    let filename = format!("changelog-{}.txt", now.format("%Y-%m-%d-%H-%M"));
+    let path = selected_instance.get_dot_minecraft_path().join("changelogs").join(&filename);
+
+    if let Some(parent) = path.parent() {
+        create_dir_all(parent).expect("Failed to create parent directories");
+    }
+
+    write(&path, &titles).expect("Failed to write file");
 }
 
 pub async fn check_for_updates(
