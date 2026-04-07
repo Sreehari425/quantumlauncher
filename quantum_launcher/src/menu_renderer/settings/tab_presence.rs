e648e3d26263a63eba8b58753ab7e8fa32d444ce
Anindya Shiddhartha <shiddhartha@tuta.io>
2026-04-07 11:35:20 +0600

    chore: state-driven toggles section

--- quantum_launcher/src/menu_renderer/settings/tab_presence.rs
+++ quantum_launcher/src/menu_renderer/settings/tab_presence.rs
@@ -42,7 +42,6 @@ impl MenuLauncherSettings {
             ]
             .spacing(5),
 
-
             column![
                 widget::text("Custom Presence"),
                 widget::text("Changes will take effect on launcher restart or with the press of the button below.").size(12).style(tsubtitle),
@@ -66,10 +65,17 @@ impl MenuLauncherSettings {
                 widget::Space::with_height(5),
                 widget::checkbox("Change presence during play/quit events", config.rich_presence_events.unwrap_or(true)).on_toggle(|n| Message::LauncherSettings(LauncherSettingsMessage::TogglePresenceEvents(n))),
                 widget::text("Disabling this will ensure that only the custom rich presence set above stays alive when you run the launcher and/or play Minecraft.").size(12).style(tsubtitle),
-                widget::Space::with_height(5),
-                widget::checkbox("Show current/last instance name", config.rich_presence_show_instance_name.unwrap_or(true)).on_toggle(|n| Message::LauncherSettings(LauncherSettingsMessage::TogglePresenceShowInstanceName(n))),
-                widget::Space::with_height(5),
-                widget::checkbox("Show Minecraft version", config.rich_presence_show_minecraft_version.unwrap_or(true)).on_toggle(|n| Message::LauncherSettings(LauncherSettingsMessage::TogglePresenceShowMinecraftVersion(n))),
+                if config.rich_presence_events.unwrap_or(true) {
+                    widget::column![
+                        widget::Space::with_height(5),
+                                        widget::checkbox("Show current/last instance name", config.rich_presence_show_instance_name.unwrap_or(true)).on_toggle(|n| Message::LauncherSettings(LauncherSettingsMessage::TogglePresenceShowInstanceName(n))),
+                                        widget::Space::with_height(5),
+                                        widget::checkbox("Show Minecraft version", config.rich_presence_show_minecraft_version.unwrap_or(true)).on_toggle(|n| Message::LauncherSettings(LauncherSettingsMessage::TogglePresenceShowMinecraftVersion(n))),
+                    ].spacing(5)
+                    } else {
+                        widget::column![]
+                    }
+
             ].spacing(5)
         ])
     }
